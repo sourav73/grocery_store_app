b5212ceed00ca7e3b2ce25646f1db836198258f6
sourav <mdsehab1@gmail.com>
2024-04-01 05:02:15 +0600

    added delivery info form

--- src/app/pages/make-payment/make-payment.component.ts
+++ src/app/pages/make-payment/make-payment.component.ts
@@ -1,24 +1,30 @@
-import { Component } from '@angular/core';
-import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { Component, OnInit, inject } from '@angular/core';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
+import { CartService } from '../../shared/services/cart/cart.service';
 
 @Component({
   selector: 'app-make-payment',
   standalone: true,
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './make-payment.component.html',
-  styleUrl: './make-payment.component.scss'
+  styleUrl: './make-payment.component.scss',
 })
-export class MakePaymentComponent {
+export class MakePaymentComponent implements OnInit {
+  cartService = inject(CartService);
   billingForm: FormGroup = new FormGroup({});
+
+  ngOnInit(): void {
+    this.createBillingForm();
+  }
 
   createBillingForm() {
     this.billingForm = new FormGroup({
       name: new FormControl<string>('', [Validators.required]),
       phone: new FormControl<string>('', [Validators.required]),
-      addressOne: new FormControl<string>('', [Validators.required]),
-      addressTwo: new FormControl<string>(''),
-      zipCode: new FormControl<string>('', [Validators.required]),
-      
+      email: new FormControl<string>('', [Validators.required, Validators.email]),
+      deliveryAddress: new FormControl<string>('', [Validators.required]),
+      paymentType: new FormControl<string>('', [Validators.required]),
+      transactionId: new FormControl<string>('', [Validators.required]),
     });
   }
 }
