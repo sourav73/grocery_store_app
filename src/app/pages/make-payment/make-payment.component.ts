1c8cd515abd3c0bdf122d1e653fb71beb259269d
sourav <mdsehab1@gmail.com>
2024-04-04 01:00:01 +0600

    done payment form

--- src/app/pages/make-payment/make-payment.component.ts
+++ src/app/pages/make-payment/make-payment.component.ts
@@ -1,20 +1,27 @@
 import { Component, OnInit, inject } from '@angular/core';
 import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 import { CartService } from '../../shared/services/cart/cart.service';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-make-payment',
   standalone: true,
-  imports: [ReactiveFormsModule],
+  imports: [ReactiveFormsModule, NgClass],
   templateUrl: './make-payment.component.html',
   styleUrl: './make-payment.component.scss',
 })
 export class MakePaymentComponent implements OnInit {
   cartService = inject(CartService);
   billingForm: FormGroup = new FormGroup({});
+  isTransactionHidden = true;
 
   ngOnInit(): void {
     this.createBillingForm();
+    this.billingForm.get('paymentType')?.valueChanges.subscribe((val) =>{
+      if(val){
+        this.isTransactionHidden = false;
+      }
+    });
   }
 
   createBillingForm() {
@@ -26,5 +33,9 @@ export class MakePaymentComponent implements OnInit {
       paymentType: new FormControl<string>('', [Validators.required]),
       transactionId: new FormControl<string>('', [Validators.required]),
     });
+  }
+
+  createOrder(){
+    console.log(this.billingForm.value);
   }
 }
